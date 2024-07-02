ca666ec64d2d889c03c6e840dd169779fa84be10
bbognar <bbognar@ventus.group>
2024-07-02 14:52:05 +0200

    Remove - useless logging

--- app/config/database.py
+++ app/config/database.py
@@ -1,20 +1,14 @@
-import logging
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from .settings import settings
 
-logger = logging.getLogger()
-print("!!!!!!!!!!!")
-
 if settings.DEBUG:
     SQLALCHEMY_DATABASE_URL = settings.SQLITE_CONNECTION_STRING
 else:
     SQLALCHEMY_DATABASE_URL = settings.POSTGRES_CONNECTION_STRING
 
-logger.debug(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
 )
